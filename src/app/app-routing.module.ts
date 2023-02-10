9adb8459b62ae591049d62d8449decf4b1b9b401
Mehedi Hasan Likhon <mehedi.hasansjs@gmail.com>
2023-02-10 14:13:49 +0600

    feat(core): add routes for events page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
+import { EventDetailsComponent } from './pages/event-details/event-details.component';
+import { EventListComponent } from './pages/event-list/event-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './pages/homepage/homepage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomepageComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'events',
+    component: EventListComponent,
+  },
+  {
+    path: 'events/:id',
+    component: EventDetailsComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
