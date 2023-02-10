3ce53665f22dfe29aa18b65c179419314b0252a3
Mehedi Hasan Likhon <mehedi.hasansjs@gmail.com>
2023-02-10 14:01:09 +0600

    feat(core): add firebase and configure

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'diu-cse-alumni-200',
+    appId: '1:1074828131561:web:99bbfa123d352fc5544478',
+    storageBucket: 'diu-cse-alumni-200.appspot.com',
+    apiKey: 'AIzaSyCwomLeN0yVwkNpXbmxKpfk37zyWLW8fjQ',
+    authDomain: 'diu-cse-alumni-200.firebaseapp.com',
+    messagingSenderId: '1074828131561',
+  },
+};
